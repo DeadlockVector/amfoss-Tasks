a4cb7c4054c23f29e13abc3bafed9ea7def54af7
DeadlockVector <76811055+DeadlockVector@users.noreply.github.com>
2022-12-14 23:50:38 +0530

    Add files via upload

---
+++ Task-08/index.js
@@ -0,0 +1,38 @@
+//gotta define functions
+//script connected to index.html
+//no semi colons here??
+
+let playcrash=function()
+{
+    document.getElementById("crash").play()    //repeat same thing for rest, its getElementById not getElementByID
+}
+
+let playkickbass=function()
+{
+    document.getElementById("kick_bass").play()
+}
+
+let playsnare=function()
+{
+    document.getElementById("snare_drum").play()
+}
+
+let playtom1 = function()
+{
+    document.getElementById("tom1_drum").play()
+}
+
+let playtom2 = function()
+{
+    document.getElementById("tom2_drum").play()
+}
+
+let playtom3 = function()
+{
+    document.getElementById("tom3_drum").play()
+}
+
+let playtom4 = function()
+{
+    document.getElementById("tom4_drum").play()
+}
